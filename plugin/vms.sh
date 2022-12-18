98da7611e2488d2b9ca7ca895a99c60941ea3755
TikaFlow <tika_flow@qq.com>
2022-12-18 18:26:14 +0800

    Add: rerun last command, search history command, -w option for history; Fix: get the wrong index when call history command; Change: better writing of variables, # is used for run bash command.

--- plugin/vms.sh
+++ plugin/vms.sh
@@ -8,40 +8,50 @@ info() {
 }
 
 help() {
-    if [ $# -eq 0 ]; then
-        cat man/vms.man
+    if [ "$#" -eq 0 ]; then
+        cat "${VMS_HOME}"/man/vms.man
     else
-        cat man/${1}.man
+        cat "${VMS_HOME}"/man/"${1}".man
     fi
 }
 
+his_file="${VMS_HOME}"/log/vms.history
+
 history(){
-    if [ $# -eq 0 ]; then
-        cat -n log/vms.history
+
+    if [ "$#" -eq 0 ]; then
+        cat -n "${his_file}"
         echo -e "\033[34mNote: Navigation key viewing history is currently not supported.\033[0m"
         return 0
     fi
 
-    his_paras=`getopt -o +c -n "#${FUNCNAME[0]}" -- "$@"`
-    [ $? != 0 ] && return 1
-    eval set -- ${his_paras}
+    his_paras=$(getopt -o +cw -n "${FUNCNAME[0]}" -- "$@")
+    [ "$?" != 0 ] && return 1
+    eval set -- "${his_paras}"
     while true ; do
         case "${1}" in
             -c)
                 shift
-                echo -n "" > log/vms.history
+                echo -n "" > "${his_file}"
+                return 0
+                ;;
+            -w)
+                shift
+                history_rows=$(wc -l < log/vms.history)
+                of_rows=$[history_rows-HistoryMax]
+                [ "${of_rows}" -gt 0 ] && sed -i "1,${of_rows}d" "${his_file}"
                 return 0
                 ;;
             --)
                 shift
                 ;;
             *)
-                vms_main `sed -n "${1},${1}p" log/vms.history`
-                return 1
+                [[ "${1}" = *[!0-9]* ]] && echo -e "\033[34m${1} is not a NUMBER!\033[0m" && return 2
+                vms_main $(sed -n "${1},${1}p" "${his_file}")
+                return 0
                 ;;
         esac
     done
-    echo "more..."
 }
 
 his(){
@@ -49,23 +59,33 @@ his(){
 }
 
 vms_main(){
-    echo "$@" >> log/vms.history
-    cmd=$@
-    [ `wc -l < log/vms.history` -eq 1001 ] && sed -i '1d' log/vms.history
-    if [ "!" == "${cmd:0:1}" ]; then
-        cmd=${cmd:1}
+    echo "$@" >> "${his_file}"
+    cmd="$@"
+    if [ "#" == "${cmd:0:1}" ]; then
+        cmd="${cmd:1}"
         ${cmd}
+    elif [ ":" == "${cmd:0:1}" ]; then
+        cmd="${cmd:1}"
+        : "${cmd}"
     else
         do_vms "${cmd}"
     fi
 }
 
+:(){
+    cat -n "${his_file}" | grep "$*"
+}
+
+!!(){
+    history $[$(wc -l < log/vms.history)-1]
+}
+
 do_vms() {
-    eval set -- ${cmd}
-    vms_cmd=${1}
-    [[ ${vms_cmd} = "do_vms" ]] && echo -e "\033[31mBad Command!\033[0m" && return -1
-    [[ ${vms_cmd} = "exit" ]] && unset -f exit && exit
-    if [ "$(type -t ${vms_cmd})" = "function" ]; then
+    eval set -- "${cmd}"
+    vms_cmd="${1}"
+    [[ "${vms_cmd}" = "do_vms" ]] && echo -e "\033[31mBad Command!\033[0m" && return -1
+    [[ "${vms_cmd}" = "exit" ]] && history -w && unset -f exit && exit
+    if [ "$(type -t "${vms_cmd}")" = "function" ]; then
         shift
         ${vms_cmd} "$@"
     else
